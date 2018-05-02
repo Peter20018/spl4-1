6dc923336df8dc0ed69bb33f79ab080adbaf8d77
samiandreas8 <loldetektiv01@gmail.com>
2018-05-02 11:57:01 +0200

    Schachbrett

---
+++ Schachbrett/src/schachbrett.java
@@ -0,0 +1,11 @@
+
+public class schachbrett {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("A8 B8 C8 D8 E8 F8 G8 H8\nA7 B7 C7 D7 E7 F7 G7 H7\nA6 B6 C6 D6 E6 F6 G6 H6\nA5 B5 C5 D5 E5 F5 G5 H5\nA4 B4 C4 D4 E4 F4 G4 H4\n"
+				+ "A3 B3 C3 D3 E3 F3 G3 H3\nA2 B2 C2 D2 E2 F2 G2 H2\nA1 B1 C1 D1 E1 F1 G1 H1");
+							
+	}
+
+}
