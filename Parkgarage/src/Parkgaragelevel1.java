5ff216bf9b4250a5473d1e72b99d380b8b3b6342
samiandreas8 <loldetektiv01@gmail.com>
2018-05-02 10:07:16 +0200

    pargarage level1

--- Parkgarage/src/Parkgaragelevel1.java
+++ Parkgarage/src/Parkgaragelevel1.java
@@ -1,30 +1,36 @@
-
 public class Parkgaragelevel1 {
 
 	public static void main(String[] args) {
-		int parkplaetze = 10;
-		int autos = 4;
-		String parken = "6 2 -6 3 4 -3 -4 -2";
-		String[] parken1 = parken.split(" ");
-		int anzhinein = 0;
-		int anzhinaus = 0;
-		int glchzt = 0;
-		for (int i = 0; i < parken1.length; i++) {
-			if (Integer.parseInt(parken1[i]) > 0) {
-				anzhinein++;
-				try {
-					if (Integer.parseInt(parken1[i-1])>0) {
-					glchzt = glchzt+1;
-					}
-				} catch (Exception ArrayIndexOutOfBoundsException) {
-					glchzt = 0;
-				}
-			} else if (Integer.parseInt(parken1[i]) < 0) {
-				anzhinaus++;
+
+		int parkpleatze = 100;
+		int Autos = 20;
+		String parken = "19 -19 11 -11 1 -1 17 -17 10 7 -10 -7 3 14 -14 -3 13 -13 4 12 20 18 6 16 -18 -12 -16 5 -5 -20 -6 -4 8 15 -8 2 -15 9 -2 -9";
+		String[] parkvorgaenge = parken.split(" ");
+		int maxAutosInGarage = 0;
+		int autosInGarage = 0;
+
+		for (int a = 0; a < parkvorgaenge.length; a++) {
+
+			int auto = Integer.parseInt(parkvorgaenge[a]);
+
+			if (auto < 0) {
+				System.out.println("Auto parkt aus...." + auto);
+				autosInGarage--;
+
+			} else {
+				System.out.println("Auto park ein..." + auto);
+				autosInGarage++;
+
 			}
+
+			if (maxAutosInGarage < autosInGarage) {
+				maxAutosInGarage = autosInGarage;
+
+			}
+
 		}
-		int sum = anzhinein - anzhinaus;
-		System.out.println("Die Anzahl der Parkenden Autos beträgt: " + sum + "Es waren insgesamt " + anzhinein + " Autos in der Garage. Es waren maximal "+ glchzt +" Autos gleichzeitig in der Garage.");
+		System.out.println("Es sind" +maxAutosInGarage);
+
 	}
 
 }
